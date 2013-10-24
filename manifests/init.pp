e4a9489001a30723f4957d3cd725d7cddb77c41e
Peter Pouliot <ppouliot@gmail.com>
2013-10-24 01:30:23 -0400

    adding centos support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,19 @@
-  # == Class: docker
-  #
-  # Full description of class docker here.
-  #
-  # === Parameters
-  #
-  # Document parameters here.
-  #
-  # [*sample_parameter*]
-  #   Explanation of what this parameter affects and what it defaults to.
-  #   e.g. "Specify one or more upstream ntp servers as an array."
-  #
-  # === Variables
-  #
-  # Here you should define a list of variables that this module would require.
-  #
+# == Class: docker
+#
+# Full description of class docker here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
 # [*sample_variable*]
 #   Explanation of how this variable affects the funtion of this class and if
 #   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
@@ -36,46 +36,17 @@
 # Copyright 2013 Your name here, unless otherwise noted.
 #
 
-class docker {
-  case $lsbdistid {
-    'Ubuntu': {
+class docker (
+  $required_kernel = $docker::params::required_kernel
+  $docker          = $docker::params::docker
+)inherits docker::params {
 
-      notify {"${lsbdisdescription} Docker Installation":}
+  package { "${required_kernel}":
+    ensure => latest
+  } 
 
-      if $lsbdistrelease == '12.04'{
-        $required_kernel = ['linux-image-generic-lts-raring',
-                            'linux-headers-generic-lts-raring']
-      }
-  
-      if $lsbdistrelease == '13.10' {
-        $required_kernel = "linux-image-extra-${kernelrelease}"
-      }
-
-      package { "${required_kernel}":
-        ensure => latest
-      } 
-
-      apt::key {'docker':
-        key_source => 'http://get.docker.io/gpg',
-      }
-
-      apt::source{'docker':
-        location    => 'http://get.docker.io/ubuntu',
-        release     => 'docker',
-        repos       => 'main',
-        include_src => false,
-      }
-  
-      package {'lxc-docker':
-        ensure => latest,
-        require => [
-          Package["${required_kernel}"],
-          Apt::Key['docker'],
-          Apt::Source['docker']],
-      }
-    }
-    default: {
-      warning('Unsupported Distribution')
-    }
+  package {"${docker}:
+    ensure => latest,
+    require => [ Package[$required_kernel], $docker_requirements ],
   }
 }
