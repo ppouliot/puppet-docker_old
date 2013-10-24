f57dd37137b17ce8187de15b470e801014715a6f
Peter Pouliot <ppouliot@gmail.com>
2013-10-24 01:43:51 -0400

    adding bits

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,47 +1,10 @@
-  # == Class: docker
-  #
-  # Full description of class docker here.
-  #
-  # === Parameters
-  #
-  # Document parameters here.
-  #
-  # [*sample_parameter*]
-  #   Explanation of what this parameter affects and what it defaults to.
-  #   e.g. "Specify one or more upstream ntp servers as an array."
-  #
-  # === Variables
-  #
-  # Here you should define a list of variables that this module would require.
-  #
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# == Class: docker::params
 #
-# === Examples
 #
-#  class { docker:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2013 Your name here, unless otherwise noted.
-#
+class docker::params {
+  case $operatingsystem {
 
-class docker {
-  case $lsbdistid {
     'Ubuntu': {
-
-      notify {"${lsbdisdescription} Docker Installation":}
-
       if $lsbdistrelease == '12.04'{
         $required_kernel = ['linux-image-generic-lts-raring',
                             'linux-headers-generic-lts-raring']
@@ -73,22 +36,29 @@ class docker {
       ]
     }
     'CentOS','RedHat','ScientificLinux': {
-      package {'epel-release-6.8':
-        ensure   => installed,
-        provider => rpm,
-        source   => "http://dl.fedoraproject.org/pub/epel/6/${architecture}/epel-release-6-8.noarch.rpm",
+      if $operatingsystemmajrelease == '6' {
+
+        package {'epel-release-6.8':
+          ensure   => installed,
+          provider => rpm,
+          source   => "http://dl.fedoraproject.org/pub/epel/6/${architecture}/epel-release-6-8.noarch.rpm",
+        }
+
+        yumrepo {'www.hop5.in Centos Repository'
+          name => 'hop5',
+          baseurl => 'http://www.hop5.in/yum/el6/',
+          gpgcheck => 0,
+        }
+
+        $required_kernel = "kernel-ml-aufs"
+        $docker          = "docker-io"
+        $docker_requirements = [
+          Package['epel-release-6.8'],
+          Yumrepo['www.hop5.in Centos Repository']
+        ]
+      } else {
+        warning("Unsupported ${operatingsystem} version ${operatingsystemrelease}")
       }
-      yumrepo {'www.hop5.in Centos Repository'
-        name => 'hop5',
-        baseurl => 'http://www.hop5.in/yum/el6/',
-        gpgcheck => 0,
-      }
-      $required_kernel = "kernel-ml-aufs"
-      $docker          = "docker-io"
-      $docker_requirements = [
-        Package['epel-release-6.8'],
-        Yumrepo['www.hop5.in Centos Repository']
-      ]
     }
     default: {
       warning('Unsupported Distribution')
